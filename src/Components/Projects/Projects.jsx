aa110758d31dfbb5b544f910b3cf474a40433ae7
Bruno-Desktop <brunobarbaran@hotmail.com>
2024-07-20 22:32:47 -0500

    Add new proyect

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -8,6 +8,8 @@ import Python from '../../Icons/Python'
 import HuggingFace from '../../Icons/HuggingFace'
 import JavaScript from '../../Icons/JavaScript'
 import MicrosoftSQLServer from '../../Icons/SQLServer'
+import ReactIcon from '../../Icons/ReactIcon'
+import FirebaseIcon from '../../Icons/FirebaseIcon'
 
 const Projects = ({ setCurrentSection }) => {
 
@@ -16,7 +18,7 @@ const Projects = ({ setCurrentSection }) => {
             id: 1,
             name: "Python",
             className: "bg-blue text-white",
-            icon: <Python width={16} height={16} />
+            icon: <Python width={12} height={10} />
         },
         HUGGING_FACE: {
             id: 2,
@@ -36,6 +38,18 @@ const Projects = ({ setCurrentSection }) => {
             className: "bg-blue text-white",
             icon: <MicrosoftSQLServer width={10} height={10} />
         },
+        REACT: {
+            id: 5,
+            name: "React",
+            className: "bg-blue text-white",
+            icon: <ReactIcon width={10} height={10} />
+        },
+        FIREBASE: {
+            id: 6,
+            name: "Firebase",
+            className: "bg-blue text-white",
+            icon: <FirebaseIcon width={10} height={10} />
+        },
     }
 
     const PROjECTS = [
@@ -51,11 +65,20 @@ const Projects = ({ setCurrentSection }) => {
         {
             id: 2,
             title: "Georeferencia en la Gestión Ambiental",
-            description: "Desarrollo de un sistema integral utilizando Google Maps para la localización de estaciones de monitoreo y puntos de acopio, además de realizar seguimiento meteorológico. Incluye una plataforma de visualización de datos con diagramas Stiff para análisis de calidad del agua, y mapas de calor para gestionar situaciones de excedencia.",
+            description: "Desarrollé un sistema integral con Google Maps para localizar estaciones de monitoreo y puntos de acopio, además de realizar seguimiento meteorológico. Incluye una plataforma de visualización de datos con diagramas Stiff para análisis de calidad del agua y mapas de calor para gestionar situaciones de excedencia.",
             link: "https://www.linkedin.com/posts/eco2biz_la-importancia-de-la-informaci%C3%B3n-georeferencial-activity-7163396183825047553-S_X7?utm_source=share&utm_medium=member_desktop",
             github: "",
             image: "/projects/Google Maps Module.webp",
             tags: [TAGS.JAVASCRIPT, TAGS.SQLSERVER]
+        },
+        {
+            id: 3,
+            title: "Sistema de Asesorias UTP - EducaUTP",
+            description: "Diseño de una aplicación web para gestionar asesorias en la UTP, utilizando React Context para la autenticación segura y manejo avanzado de estados, Firebase para almacenamiento en tiempo real, y diseñé una interfaz intuitiva y responsive.Además, manejo de variables de entorno para keys",
+            link: "https://educautp.web.app/",
+            github: "https://github.com/iBarb/EducaUTP",
+            image: "/projects/educaUTP.webp",
+            tags: [TAGS.REACT, TAGS.FIREBASE]
         }
     ]
 
